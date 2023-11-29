2f892a2ceeeb8709dfc4afb08bb9835ac335b8ba
Amandaaaz <amandazfernandes@hotmail.com>
2023-11-29 17:50:16 -0300

    29/11

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -146,245 +146,245 @@ const Formulario = ({ perguntaAtual, avancarPergunta }) => {
   const fade = useSpring({ opacity: 1, from: { opacity: 0 } });
   const slide = useSpring({ marginLeft: '0%', from: { marginLeft: '-100%' } });
 
-  
-const toggleCheckbox = (opcao) => {
-  let opcoesSelecionadas;
+  const toggleCheckbox = (opcao) => {
+    let opcoesSelecionadas;
 
-  if (perguntas[perguntaAtual - 1].tipo === 'opcoes') {
-    // Para perguntas do tipo 'opcoes', permite apenas uma opção selecionada
-    opcoesSelecionadas = [opcao];
-  } else {
-    // Para outros tipos de perguntas, mantém a lógica original
-    opcoesSelecionadas = [...resposta];
-
-    if (opcoesSelecionadas.includes(opcao)) {
-      const index = opcoesSelecionadas.indexOf(opcao);
-      opcoesSelecionadas.splice(index, 1);
+    if (perguntas[perguntaAtual - 1].tipo === 'opcoes') {
+      // Para perguntas do tipo 'opcoes', permite apenas uma opção selecionada
+      opcoesSelecionadas = [opcao];
     } else {
-      opcoesSelecionadas.push(opcao);
+      // Para outros tipos de perguntas, mantém a lógica original
+      opcoesSelecionadas = [...resposta];
+
+      if (opcoesSelecionadas.includes(opcao)) {
+        const index = opcoesSelecionadas.indexOf(opcao);
+        opcoesSelecionadas.splice(index, 1);
+      } else {
+        opcoesSelecionadas.push(opcao);
+      }
     }
-  }
 
-  setResposta(opcoesSelecionadas);
-  setMostrarErro(false);
-};
+    setResposta(opcoesSelecionadas);
+    setMostrarErro(false);
+  };
 
-const renderPergunta = (pergunta) => {
-  switch (pergunta.tipo) {
-    case 'opcoes':
-      return pergunta.texto === 'Você deseja começar com CPF ou CNPJ?' ? (
-        <>
-          {renderOpcoes(pergunta.opcoes)}
-          {(resposta.includes('CPF') || resposta.includes('CNPJ')) && (
-            <React.Fragment>
-              {resposta.includes('CPF') && (
-                <React.Fragment>
-                  <Label>Número do CPF:</Label>
-                  <Input
-                    type="text"
-                    placeholder="Digite sua resposta"
-                    value={resposta[1] || ''}
-                    onChange={(e) => setResposta(['CPF', e.target.value])}
-                  />
-                </React.Fragment>
-              )}
-              {resposta.includes('CNPJ') && (
-                <React.Fragment>
-                  <Label>Número do CNPJ:</Label>
-                  <Input
-                    type="text"
-                    placeholder="Digite sua resposta"
-                    value={resposta[1] || ''}
-                    onChange={(e) => setResposta(['CNPJ', e.target.value])}
-                  />
-                </React.Fragment>
-              )}
-            </React.Fragment>
-          )}
-        </>
-      ) : (
-        renderOpcoes(pergunta.opcoes)
-      );
-    case 'texto':
-      return (
-        <Input
-          type="text"
-          placeholder="Digite sua resposta"
-          value={resposta[0] || ''}
-          onChange={(e) => setResposta([e.target.value])}
-        />
-      );
-    default:
-      return null;
-  }
-};
-
-const renderOpcoes = (opcoes) => {
-  return opcoes.map((opcao, index) => (
-    <React.Fragment key={index}>
-      <label>
-        <Checkbox
-          type="checkbox"
-          name="opcao"
-          value={opcao}
-          checked={resposta.includes(opcao)}
-          onChange={() => toggleCheckbox(opcao)}
-        />
-        {opcao}
-      </label>
-      <br />
-    </React.Fragment>
-  ));
-};
-const validarCPF = (cpf) => {
-  // Remover caracteres não numéricos
-  cpf = cpf.replace(/\D/g, '');
-
-  // Verificar se o CPF tem 11 dígitos
-  if (cpf.length !== 11) {
-    return false;
-  }
-
-  // Verificar se todos os dígitos são iguais (caso contrário, CPF é inválido)
-  if (/^(\d)\1+$/.test(cpf)) {
-    return false;
-  }
-
-  // Algoritmo de validação do CPF
-  let sum = 0;
-  let remainder;
-
-  for (let i = 1; i <= 9; i++) {
-    sum += parseInt(cpf[i - 1]) * (11 - i);
-  }
-
-  remainder = (sum * 10) % 11;
-
-  if (remainder === 10 || remainder === 11) {
-    remainder = 0;
-  }
-
-  if (remainder !== parseInt(cpf[9])) {
-    return false;
-  }
-
-  sum = 0;
-
-  for (let i = 1; i <= 10; i++) {
-    sum += parseInt(cpf[i - 1]) * (12 - i);
-  }
-
-  remainder = (sum * 10) % 11;
-
-  if (remainder === 10 || remainder === 11) {
-    remainder = 0;
-  }
-
-  if (remainder !== parseInt(cpf[10])) {
-    return false;
-  }
-
-  return true;
-};
-const validarCNPJ = (cnpj) => {
-  // Remover caracteres não numéricos
-  cnpj = cnpj.replace(/\D/g, '');
-
-  // Verificar se o CNPJ tem 14 dígitos
-  if (cnpj.length !== 14) {
-    return false;
-  }
-
-  // Verificar se todos os dígitos são iguais (caso contrário, CNPJ é inválido)
-  if (/^(\d)\1+$/.test(cnpj)) {
-    return false;
-  }
-
-  // Algoritmo de validação do CNPJ
-  let size = cnpj.length - 2;
-  let numbers = cnpj.substring(0, size);
-  let digits = cnpj.substring(size);
-  let sum = 0;
-  let pos = size - 7;
-
-  for (let i = size; i >= 1; i--) {
-    sum += parseInt(numbers.charAt(size - i)) * pos--;
-    if (pos < 2) {
-      pos = 9;
+  const renderPergunta = (pergunta) => {
+    switch (pergunta.tipo) {
+      case 'opcoes':
+        return pergunta.texto === 'Você deseja começar com CPF ou CNPJ?' ? (
+          <>
+            {renderOpcoes(pergunta.opcoes)}
+            {(resposta.includes('CPF') || resposta.includes('CNPJ')) && (
+              <React.Fragment>
+                {resposta.includes('CPF') && (
+                  <React.Fragment>
+                    <Label>Número do CPF:</Label>
+                    <Input
+                      type="text"
+                      placeholder="Digite sua resposta"
+                      value={resposta[1] || ''}
+                      onChange={(e) => setResposta(['CPF', e.target.value])}
+                    />
+                  </React.Fragment>
+                )}
+                {resposta.includes('CNPJ') && (
+                  <React.Fragment>
+                    <Label>Número do CNPJ:</Label>
+                    <Input
+                      type="text"
+                      placeholder="Digite sua resposta"
+                      value={resposta[1] || ''}
+                      onChange={(e) => setResposta(['CNPJ', e.target.value])}
+                    />
+                  </React.Fragment>
+                )}
+              </React.Fragment>
+            )}
+          </>
+        ) : (
+          renderOpcoes(pergunta.opcoes)
+        );
+      case 'texto':
+        return (
+          <Input
+            type="text"
+            placeholder="Digite sua resposta"
+            value={resposta[0] || ''}
+            onChange={(e) => setResposta([e.target.value])}
+          />
+        );
+      default:
+        return null;
     }
-  }
+  };
 
-  let result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+  const renderOpcoes = (opcoes) => {
+    return opcoes.map((opcao, index) => (
+      <React.Fragment key={index}>
+        <label>
+          <Checkbox
+            type="checkbox"
+            name="opcao"
+            value={opcao}
+            checked={resposta.includes(opcao)}
+            onChange={() => toggleCheckbox(opcao)}
+          />
+          {opcao}
+        </label>
+        <br />
+      </React.Fragment>
+    ));
+  };
 
-  if (result !== parseInt(digits.charAt(0))) {
-    return false;
-  }
+  const validarCPF = (cpf) => {
+    // Remover caracteres não numéricos
+    cpf = cpf.replace(/\D/g, '');
 
-  size = size + 1;
-  numbers = cnpj.substring(0, size);
-  sum = 0;
-  pos = size - 7;
-
-  for (let i = size; i >= 1; i--) {
-    sum += parseInt(numbers.charAt(size - i)) * pos--;
-    if (pos < 2) {
-      pos = 9;
+    // Verificar se o CPF tem 11 dígitos
+    if (cpf.length !== 11) {
+      return false;
     }
-  }
 
-  result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+    // Verificar se todos os dígitos são iguais (caso contrário, CPF é inválido)
+    if (/^(\d)\1+$/.test(cpf)) {
+      return false;
+    }
 
-  if (result !== parseInt(digits.charAt(1))) {
-    return false;
-  }
+    // Algoritmo de validação do CPF
+    let sum = 0;
+    let remainder;
 
-  return true;
-};
+    for (let i = 1; i <= 9; i++) {
+      sum += parseInt(cpf[i - 1]) * (11 - i);
+    }
 
-const handleSubmit = (event) => {
-  event.preventDefault();
+    remainder = (sum * 10) % 11;
 
-  if (perguntaAtual <= perguntas.length) {
-    if (perguntaAtual === 1 && resposta.length === 0) {
-      setMostrarErro(true);
-    } else {
-      // Verificar a validação de CPF ou CNPJ apenas na primeira pergunta
-      if (perguntaAtual === 1) {
-        if (resposta.includes('CPF') || resposta.includes('CNPJ')) {
-          const campoTexto = resposta[1] || '';
-          
-          // Verificar se o campo de texto está vazio
-          if (campoTexto.trim() === '') {
-            setMostrarErro(true);
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+
+    if (remainder !== parseInt(cpf[9])) {
+      return false;
+    }
+
+    sum = 0;
+
+    for (let i = 1; i <= 10; i++) {
+      sum += parseInt(cpf[i - 1]) * (12 - i);
+    }
+
+    remainder = (sum * 10) % 11;
+
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+
+    if (remainder !== parseInt(cpf[10])) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const validarCNPJ = (cnpj) => {
+    // Remover caracteres não numéricos
+    cnpj = cnpj.replace(/\D/g, '');
+
+    // Verificar se o CNPJ tem 14 dígitos
+    if (cnpj.length !== 14) {
+      return false;
+    }
+
+    // Verificar se todos os dígitos são iguais (caso contrário, CNPJ é inválido)
+    if (/^(\d)\1+$/.test(cnpj)) {
+      return false;
+    }
+
+    // Algoritmo de validação do CNPJ
+    let size = cnpj.length - 2;
+    let numbers = cnpj.substring(0, size);
+    let digits = cnpj.substring(size);
+    let sum = 0;
+    let pos = size - 7;
+
+    for (let i = size; i >= 1; i--) {
+      sum += parseInt(numbers.charAt(size - i)) * pos--;
+      if (pos < 2) {
+        pos = 9;
+      }
+    }
+
+    let result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+    if (result !== parseInt(digits.charAt(0))) {
+      return false;
+    }
+
+    size = size + 1;
+    numbers = cnpj.substring(0, size);
+    sum = 0;
+    pos = size - 7;
+
+    for (let i = size; i >= 1; i--) {
+      sum += parseInt(numbers.charAt(size - i)) * pos--;
+      if (pos < 2) {
+        pos = 9;
+      }
+    }
+
+    result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+    if (result !== parseInt(digits.charAt(1))) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (perguntaAtual <= perguntas.length) {
+      const perguntaAtualInfo = perguntas[perguntaAtual - 1];
+
+      if (perguntaAtual === 1 && resposta.length === 0) {
+        setMostrarErro('O campo está vazio');
+        return;
+      }
+
+      if (perguntaAtual === 1 && (resposta.includes('CPF') || resposta.includes('CNPJ'))) {
+        const campoTexto = resposta[1] || '';
+
+        // Verificar se o campo de texto está vazio
+        if (!campoTexto.trim()) {
+          setMostrarErro('O campo está vazio');
+          return;
+        }
+
+        if (resposta.includes('CPF')) {
+          const cpfValido = validarCPF(campoTexto);
+          if (!cpfValido) {
+            setMostrarErro('CPF inválido');
             return;
           }
-
-          if (resposta.includes('CPF')) {
-            const cpfValido = validarCPF(campoTexto);
-            if (!cpfValido) {
-              setMostrarErro(true);
-              return;
-            }
-          } else if (resposta.includes('CNPJ')) {
-            const cnpjValido = validarCNPJ(campoTexto);
-            if (!cnpjValido) {
-              setMostrarErro(true);
-              return;
-            }
+        } else if (resposta.includes('CNPJ')) {
+          const cnpjValido = validarCNPJ(campoTexto);
+          if (!cnpjValido) {
+            setMostrarErro('CNPJ inválido');
+            return;
           }
         }
       }
 
       avancarPergunta();
       setMostrarErro(false);
+    } else {
+      setConcluido(true);
     }
-  } else {
-    setConcluido(true);
-  }
 
-  setResposta([]);
-};
-
+    setResposta([]);
+  };
 
   return (
     <FormContainer style={fade}>
@@ -399,7 +399,7 @@ const handleSubmit = (event) => {
               <React.Fragment>
                 <Label>{perguntas[perguntaAtual - 1].texto}</Label>
                 {renderPergunta(perguntas[perguntaAtual - 1])}
-                {mostrarErro && <MensagemErro>Selecione uma opção</MensagemErro>}
+                {mostrarErro && <MensagemErro>{mostrarErro}</MensagemErro>}
               </React.Fragment>
             )}
             <Button
